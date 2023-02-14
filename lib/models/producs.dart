1a581f85ff81bfb8820cc6bb6fecaf20a3c67b24
Bruno Codeman <brunobemfica@gmail.com>
2023-02-14 00:47:53 -0300

    Adding more fields to product

--- lib/models/producs.dart
+++ lib/models/producs.dart
@@ -1,4 +1,16 @@
+import 'package:flutter/foundation.dart';
+
 class Product {
   final String id;
-  const Product(this.id);
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite = false;
+  Product(
+      {@required this.id,
+      @required this.title,
+      @required this.description,
+      @required this.price,
+      @required this.imageUrl});
 }
